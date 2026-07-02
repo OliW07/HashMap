12f4f54a1a14217f523b35985aa9e61526283fe6
Oliver White <oliverjwhite07@gmail.com>
2026-07-02 13:01:50 +1200

    Format StateSet to std::string

--- include/detail/states.h
+++ include/detail/states.h
@@ -1,20 +1,53 @@
 #pragma once
 
+#include <string>
 #include <cstdint>
+#include <initializer_list>
 
-enum class State { null = 0, empty = 1 << 0, occupied = 1 << 1, deleted = 1 << 2};
+enum class State { empty = 1 << 0, occupied = 1 << 1, deleted = 1 << 2};
+
+inline std::string to_string(State state) {
+    switch(state){
+        case State::empty:
+            return "State::empty";
+        case State::occupied:
+            return "State::occupied";
+        case State::deleted:
+            return "State::deleted";
+        default:
+            return "unknown state";
+
+    }
+}
 
 class StateSet {
     private:
-        uint8_t bits_;
-
-
+        uint8_t bits_ = 0;
 
     public:
-        StateSet() : bits_(0) {}
 
-        explicit StateSet(State s1 = State::null, State s2 = State::null, State s3=State::null){
-            bits_ = static_cast<uint8_t>(s1) | static_cast<uint8_t>(s2) | static_cast<uint8_t>(s3);
+        StateSet() = default;
+        StateSet(std::initializer_list<State> states){
+            for(State state : states){
+                bits_ |= static_cast<uint8_t>(state);
+            }
+        }
+
+        bool contains(State state) const {
+            return (bits_ & static_cast<uint8_t>(state)) != 0;
+        }
+
+        std::string format() const {
+            std::string res;
+            for(State state : {State::empty, State::deleted, State::occupied}){
+                if(contains(state)){
+                    if(!res.empty())
+                        res += " | ";
+                    res += to_string(state);
+                }
+            }
+
+            return res;
         }
 
 
